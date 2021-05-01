0691ebb1191c06e8cd541dda9e25376d76816a55
Hanna <hannaeberhoefer@yahoo.com>
2021-05-01 11:59:01 +0100

    Appt-planner step 4 - duplicate error

--- appt-planner/src/components/contactForm/ContactForm.js
+++ appt-planner/src/components/contactForm/ContactForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 export const ContactForm = ({
   name,
@@ -10,6 +10,14 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      <input type='text' name='name' value={name} onChange={e => setName(e.target.value)}
+              placeholder='Your Name' required />
+      <input type='tel' name='phone' value={phone} onChange={e => setPhone(e.target.value)}
+              pattern='((\+44(\s\(0\)\s|\s0\s|\s)?)|0)7\d{3}(\s)?\d{6}' placeholder='Phone Number' required />
+      <input type='email' name='email' value={email} onChange={e => setEmail(e.target.value)}
+              pattern="^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$" placeholder='Your Email' required />
+      <input type='submit' value='Add Contact' />
+    </form>
   );
 };
--- appt-planner/src/containers/contactsPage/ContactsPage.js
+++ appt-planner/src/containers/contactsPage/ContactsPage.js
@@ -1,28 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { ContactForm } from '../../components/contactForm/ContactForm.js';
+import { TileList } from '../../components/tileList/TileList.js';
 
-export const ContactsPage = () => {
-  /*
-  Define state variables for 
-  contact info and duplicate check
-  */
+export const ContactsPage = ( { contacts, addContact } ) => {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [duplicate, setDuplicate] = useState(false);
+  
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    /*
-    Add contact info and clear data
-    if the contact name is not a duplicate
-    */
+    if (!duplicate) {
+      addContact(name, phone, email);
+      console.log(name)
+      setName('');
+      setPhone('');
+      setEmail('');
+    }
   };
 
-  /*
-  Using hooks, check for contact name in the 
-  contacts array variable in props
-  */
+  useEffect(() => {
+    if (contacts.includes(name)) {
+      setDuplicate(true);
+    } else {
+      setDuplicate(false);
+    }
+  }, [contacts, name, duplicate]);
 
   return (
     <div>
       <section>
         <h2>Add Contact</h2> 
+        <ContactForm name={name} setName={setName}
+                    phone={phone} setPhone={setPhone}
+                    email={email} setEmail={setEmail}
+                    handleSubmit={handleSubmit}
+        /> 
       </section>
       <hr />
       <section>
