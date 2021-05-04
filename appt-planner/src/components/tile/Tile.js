2735d04b3d80c4f17f7d55ac0e534fca650353fd
Hanna <hannaeberhoefer@yahoo.com>
2021-05-04 16:27:45 +0100

    Appt-planner step 6

--- appt-planner/src/components/tile/Tile.js
+++ appt-planner/src/components/tile/Tile.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({ tile }) => {
   return (
     <div className="tile-container">
-      
+      {Object.values(tile).map((value, index) => {
+        if (index === 0) {
+          return <p className='tile-title' key={index}>{value}</p>
+        } else {
+          return <p clasName='tile' key={index}>{value}</p>
+        }
+      })}
     </div>
   );
 };
