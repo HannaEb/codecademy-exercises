bed1fe40fb4350995dd66f45d970156e4ff94a94
Hanna <hannaeberhoefer@yahoo.com>
2021-05-10 11:02:52 +0100

    Display contacts

--- appt-planner/src/components/tile/Tile.js
+++ appt-planner/src/components/tile/Tile.js
@@ -1,15 +1,14 @@
 import React from "react";
 
 export const Tile = ({ tile }) => {
+ 
   return (
     <div className="tile-container">
-      {Object.values(tile).map((value, index) => {
-        if (index === 0) {
-          return <p className='tile-title' key={index}>{value}</p>
-        } else {
-          return <p clasName='tile' key={index}>{value}</p>
-        }
-      })}
-    </div>
+    {Object.values(tile).map((value, index) => (
+      <p key={index} className={`${index === 0 ? "tile-title" : ""} tile`}>
+        {value}
+      </p>
+    ))}
+  </div>
   );
 };
